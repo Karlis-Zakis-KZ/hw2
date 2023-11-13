ef1ff508e3ad1f65301afb955b51ac8f961f82c4
Karlis-Zakis-KZ <karliszakis13@gmail.com>
2023-11-13 22:40:30 +0200

    Checking if now works

---
+++ matmul.s
@@ -0,0 +1,11 @@
+.text
+.align 2
+.global matmul
+.type matmul, %function
+
+@ str r1, [r5, #0] ---- stores r1 into *r5[0]
+
+@ int matmul(int h1, int w1, int *m1, int h2, int w2, int *m2, int *m3);
+matmul:
+  stmfd sp!, {r0-r12, lr}
+@ sp -> r0, r1, r2, r3, r4, r5 ,r6 ,r7, r8, r9, r10, r11, r12, lr
