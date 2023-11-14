5c4ff5171873a472bce5bc85519392c58acce145
Karlis-Zakis-KZ <karliszakis13@gmail.com>
2023-11-14 20:04:58 +0200

    412341

--- matmul.s
+++ matmul.s
@@ -10,62 +10,62 @@ matmul:
     mov r5, #0
     mov r6, #0
 
-loop_i:
+for_i:
     ldr r2, [sp, #0]
     cmp r4, r2
-    bge end_loop_i
+    bge end_for_i
 
     mov r5, #0
 
-loop_j:
+for_j:
     ldr r3, [sp, #56]
     cmp r5, r3
-    bge end_loop_j
+    bge end_for_j
 
     mov r6, #0
 
-loop_k:
+for_k:
     ldr r1, [sp, #12]
     cmp r6, r1
-    bge end_loop_k
+    bge end_for_k
 
     mov r9, r4, LSL #2
     mul r7, r9, r1
-    mov r8, r6, LSL #2
-    add r7, r7, r8
+    mov r10, r6, LSL #2
+    add r7, r7, r10
 
     ldr r0, [sp, #8]
     ldr r1, [r0, r7]
 
-    mul r7, r8, r3
+    mul r7, r10, r3
     mov r9, r5, LSL #2
     add r7, r7, r9
     ldr r0, [sp, #60]
     ldr r2, [r0, r7]
 
-    mul r10, r1, r2
+    mul r11, r1, r2
 
     ldr r0, [sp, #64]
     mov r9, r4, LSL #2
     mul r7, r9, r3
-    mov r11, r5, LSL #2
-    add r7, r7, r11
+    mov r12, r5, LSL #2
+    add r7, r7, r12
 
     ldr r0, [r0, r7]
-    add r1, r10, r0
+    add r1, r11, r0
     str r1, [r0, r7]
 
     add r6, r6, #1
-    b loop_k
+    b for_k
 
-end_loop_k:
+end_for_k:
     add r5, r5, #1
-    b loop_j
+    b for_j
 
-end_loop_j:
+end_for_j:
     add r4, r4, #1
-    b loop_i
+    b for_i
 
-end_loop_i:
+end_for_i:
     ldmfd sp!, {r0-r12, lr}
     bx lr
