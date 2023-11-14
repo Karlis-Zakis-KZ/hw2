037c89ffc8d7511f58a0ffa4b13aa690182afc47
Karlis-Zakis-KZ <karliszakis13@gmail.com>
2023-11-14 19:35:31 +0200

    ddd

--- matmul.s
+++ matmul.s
@@ -4,59 +4,70 @@
 .type matmul, %function
 
 matmul:
-  stmfd sp!, {r0-r12, lr}
+    stmfd sp!, {r0-r12, lr}
 
-  ldr r3, [sp, #0]  @ h1
-  ldr r4, [sp, #56]  @ w2
-  ldr r2, [sp, #12]  @ h2
-  ldr r0, [sp, #8]  @ *m1
-  ldr r1, [sp, #60]  @ *m2
-  ldr r12, [sp, #64]  @ *m3
+    mov r5, #0          // Initialize loop counters
+    mov r6, #0
+    mov r7, #0
 
-  mov r5, #0 
+    ldr r3, [sp, #0]    // Load matrix dimensions
+    ldr r4, [sp, #56]
+    ldr r2, [sp, #12]
+
 for_i:
-  cmp r5, r3 
-  bge end_for_i
-  mov r6, #0 
+    cmp r5, r3          // Check if outer loop is complete
+    bge end_for_i
+
+    mov r6, #0          // Initialize inner loop counters
 
 for_j:
-  cmp r6, r4 
-  bge end_for_j
-  mov r7, #0 
+    cmp r6, r4
+    bge end_for_j
+
+    mov r7, #0
 
 for_k:
-  cmp r7, r2 
-  bge end_for_k
+    cmp r7, r2
+    bge end_for_k
 
-  mov r10, r5, LSL#2 
-  mul r8, r10, r2 
-  add r8, r8, r7, LSL#2
-  ldr r2, [r0, r8] 
+    mov r10, r5, LSL #2   // Calculate offsets for matrix elements
+    mul r8, r10, r2
+    mov r9, r7, LSL #2
+    add r8, r8, r9
 
-  mul r8, r7, r4 
-  add r8, r8, r6, LSL#2
-  ldr r3, [r1, r8] 
+    ldr r0, [sp, #8]    // Load matrices
+    ldr r2, [r0, r8]
 
-  mul r11, r2, r3
+    mul r8, r9, r4
+    mov r10, r6, LSL #2
+    add r8, r8, r10
 
-  mov r10, r5, LSL#2 
-  mul r8, r10, r4 
-  add r8, r8, r6, LSL#2
-  ldr r1, [r12, r8] 
-  add r2, r11, r1  
-  str r2, [r12, r8] 
+    ldr r1, [sp, #60]
+    ldr r3, [r1, r8]
 
-  add r7, r7, #1 
-  b for_k
+    mul r11, r2, r3        // Calculate element product
+
+    ldr r0, [sp, #64]    // Destination matrix
+    mov r10, r5, LSL #2
+    mul r8, r10, r4
+    mov r12, r6, LSL #2
+    add r8, r8, r12
+
+    ldr r1, [r0, r8]
+    add r2, r11, r1        // Update destination element
+    str r2, [r0, r8]
+
+    add r7, r7, #1          // Increment loop counters
+    b for_k
 
 end_for_k:
-  add r6, r6, #1 
-  b for_j
+    add r6, r6, #1
+    b for_j
 
 end_for_j:
-  add r5, r5, #1 
-  b for_i
+    add r5, r5, #1
+    b for_i
 
 end_for_i:
-  ldmfd sp!, {r0-r12, lr}
-  bx lr
+    ldmfd sp!, {r0-r12, lr}
+    bx lr
