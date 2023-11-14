ec4723cd59003500162532fd923aa2f9995cbb86
Karlis-Zakis-KZ <karliszakis13@gmail.com>
2023-11-14 19:30:39 +0200

    g

--- matmul.s
+++ matmul.s
@@ -10,59 +10,60 @@ matmul:
   mov r6, #0 
   mov r7, #0 
 
-for_i:
-  ldr r3, [sp, #0]
+loop_i:
+  ldr r2, [sp, #0]
+  cmp r4, r2 
+  bge end_loop_i
+  mov r5, #0 
+
+loop_j:
+  ldr r3, [sp, #56]
   cmp r5, r3 
-  bge end_for_i
+  bge end_loop_j
   mov r6, #0 
 
-for_j:
-  ldr r4, [sp, #56]
-  cmp r6, r4 
-  bge end_for_j
-  mov r7, #0 
+loop_k:
+  ldr r1, [sp, #12]
+  cmp r6, r1 
+  bge end_loop_k
 
-for_k:
-  ldr r2, [sp, #12]
-  cmp r7, r2 
-  bge end_for_k
-
-  mov r10, r5, LSL#2 
-  mul r8, r10, r2 
-  mov r9, r7, LSL#2
-  add r8, r8, r9 
+  mov r9, r4, LSL#2 
+  mul r7, r9, r1 
+  mov r8, r6, LSL#2
+  add r7, r7, r8 
   ldr r0, [sp, #8] 
-  ldr r2, [r0, r8] 
+  ldr r1, [r0, r7] 
 
-  mul r8, r9, r4 
-  mov r10, r6, LSL#2 
-  add r8, r8, r10 
-  ldr r1, [sp, #60] 
-  ldr r3, [r1, r8] 
+  mul r7, r8, r3 
+  mov r9, r5, LSL#2 
+  add r7, r7, r9 
+  ldr r0, [sp, #60] 
+  ldr r2, [r0, r7] 
 
-  mul r11, r2, r3 
+  mul r10, r1, r2 
 
   ldr r0, [sp, #64] 
-  mov r10, r5, LSL#2 
-  mul r8, r10, r4 
-  mov r12, r6, LSL#2 
-  add r8, r8, r12 
+  mov r9, r4, LSL#2 
+  mul r7, r9, r3 
+  mov r11, r5, LSL#2 
+  add r7, r7, r11 
 
-  ldr r1, [r0, r8] 
-  add r2, r11, r1  
-  str r2, [r0, r8] 
+  ldr r0, [r0, r7] 
+  add r1, r10, r0  
+  str r1, [r0, r7] 
 
-  add r7, r7, #1 
-  b for_k
-
-end_for_k:
   add r6, r6, #1 
-  b for_j
+  b loop_k
 
-end_for_j:
+end_loop_k:
   add r5, r5, #1 
-  b for_i
+  b loop_j
 
-end_for_i:
+end_loop_j:
+  add r4, r4, #1 
+  b loop_i
+
+end_loop_i:
   ldmfd sp!, {r0-r12, lr}
   bx lr
+  
