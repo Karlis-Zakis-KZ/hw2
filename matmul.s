5cae7caea838a19cba76aacea31d845ecb76c3bc
Karlis-Zakis-KZ <karliszakis13@gmail.com>
2023-11-14 19:23:00 +0200

    v

--- matmul.s
+++ matmul.s
@@ -4,7 +4,7 @@
 .type matmul, %function
 
 matmul:
-    stmfd sp!, {r0-r12, lr}  @ Save registers to the stack
+    stmfd sp!, {r4-r11, lr}  @ Save registers to the stack
 
     mov r4, r0  @ h1
     mov r5, r1  @ w1
@@ -24,21 +24,25 @@ loop_j:
     mov r2, #0  @ Initialize row index for m1
     mov r3, #0  @ Initialize column index for m2
     mov lr, r9  @ Save start of row for m2
+    mov ip, #0  @ Initialize accumulator for dot product
 
 loop_k:
     ldr r12, [r11, r2, lsl #2]  @ Load element from m1
-    ldr ip, [lr, r3, lsl #2]  @ Load element from m2
-    mul r12, r12, ip  @ Multiply elements
-    str r12, [r10, r1, lsl #2]  @ Store result in m_final
+    ldr r14, [lr, r3, lsl #2]  @ Load element from m2
+    mla ip, r12, r14, ip  @ Multiply elements and add to accumulator
     add r3, r3, #1  @ Increment column index for m2
-    add r1, r1, #1  @ Increment row index for m_final
-    cmp r3, r5  @ Check if column index for m2 exceeds matrix width
+    add r2, r2, #1  @ Increment row index for m1
+    cmp r2, r5  @ Check if row index for m1 exceeds matrix width
     blt loop_k  @ Branch back if not done
 
+    str ip, [r10, r1, lsl #2]  @ Store result in m_final
     add r1, r1, #1  @ Increment row index of m_final
-    add r11, r11, r5, lsl #2  @ Increment pointer to m1 by its width
     cmp r1, r8  @ Check if row index for m_final exceeds matrix width
     blt loop_j  @ Branch back if not done
 
     add r0, r0, #1  @ Increment loop counter
     add r6, r6, r5, lsl #2  @ Increment pointer to m1 by its height
+    cmp r0, r4  @ Check if loop counter exceeds matrix height
+    blt loop_i  @ Branch back if not done
+
+    ldmfd sp!, {r4-r11, pc}  @ Restore registers from the stack
