0c01a9ab337b29763f399788bc09f78d9ccdaaf8
Karlis-Zakis-KZ <karliszakis13@gmail.com>
2023-11-14 20:22:50 +0200

    5555

--- matmul.s
+++ matmul.s
@@ -4,68 +4,70 @@
 .type matmul, %function
 
 matmul:
-    stmfd sp!, {r0-r12, lr}
+    stmfd sp!, {r0-r12, lr}  // Save registers to the stack
 
-    mov r4, #0
-    mov r5, #0
-    mov r6, #0
+    mov r5, #0  // Initialize i
+    mov r6, #0  // Initialize j
+    mov r7, #0  // Initialize k
 
-loop_i:
-    ldr r2, [sp, #0]
-    cmp r4, r2
-    bge end_loop_i
+i_loop:
+    ldr r3, [sp, #0]  // Load matrix1 row count
+    cmp r5, r3  // Compare i with matrix1 row count
+    bge end_i_loop  // If i >= matrix1 row count, exit i loop
 
-    mov r5, #0
+    mov r6, #0  // Initialize j
 
-loop_j:
-    ldr r3, [sp, #56]
-    cmp r5, r3
-    bge end_loop_j
+j_loop:
+    ldr r4, [sp, #56]  // Load matrix2 column count
+    cmp r6, r4  // Compare j with matrix2 column count
+    bge end_j_loop  // If j >= matrix2 column count, exit j loop
 
-    mov r6, #0
+    mov r7, #0  // Initialize k
 
-loop_k:
-    ldr r1, [sp, #12]
-    cmp r6, r1
-    bge end_loop_k
+k_loop:
+    ldr r2, [sp, #12]  // Load matrix1 column count (or matrix2 row count)
+    cmp r7, r2  // Compare k with matrix1 column count
+    bge end_k_loop  // If k >= matrix1 column count, exit k loop
 
-    mov r9, r4, LSL #2
-    mul r7, r9, r1
-    mov r8, r6, LSL #2
-    add r7, r7, r8
-
+    // Calculate the offset for matrix1[i][k] and load the value
+    mov r10, r5, LSL #2
+    mul r8, r10, r2
+    mov r9, r7, LSL #2
+    add r8, r8, r9
     ldr r0, [sp, #8]
-    ldr r1, [r0, r7]
+    ldr r2, [r0, r8]
 
-    mul r7, r8, r3
-    mov r9, r5, LSL #2
-    add r7, r7, r9
-    ldr r0, [sp, #60]
-    ldr r2, [r0, r7]
+    // Calculate the offset for matrix2[k][j] and load the value
+    mul r8, r9, r4
+    mov r10, r6, LSL #2
+    add r8, r8, r10
+    ldr r1, [sp, #60]
+    ldr r3, [r1, r8]
 
-    mul r10, r1, r2
+    // Multiply matrix1[i][k] and matrix2[k][j]
+    mul r11, r2, r3
 
+    // Calculate the offset for result[i][j], load the value, add the multiplication result, and store the new value
     ldr r0, [sp, #64]
-    mov r9, r4, LSL #2
-    mul r7, r9, r3
-    mov r11, r5, LSL #2
-    add r7, r7, r11
+    mov r10, r5, LSL #2
+    mul r8, r10, r4
+    mov r12, r6, LSL #2
+    add r8, r8, r12
+    ldr r1, [r0, r8]
+    add r2, r11, r1
+    str r2, [r0, r8]
 
-    ldr r0, [r0, r7]
-    add r1, r10, r0
-    str r1, [r0, r7]
+    add r7, r7, #1  // Increment k
+    b k_loop  // Repeat k loop
 
-    add r6, r6, #1
-    b loop_k
+end_k_loop:
+    add r6, r6, #1  // Increment j
+    b j_loop  // Repeat j loop
 
-end_loop_k:
-    add r5, r5, #1
-    b loop_j
+end_j_loop:
+    add r5, r5, #1  // Increment i
+    b i_loop  // Repeat i loop
 
-end_loop_j:
-    add r4, r4, #1
-    b loop_i
-
-end_loop_i:
-    ldmfd sp!, {r0-r12, lr}
-    bx lr
+end_i_loop:
+    ldmfd sp!, {r0-r12, lr}  // Restore registers from the stack
+    bx lr  // Return from function
