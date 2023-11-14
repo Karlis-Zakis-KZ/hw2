f5ce0064308bda5157aaf385fe8070a680be690a
Karlis-Zakis-KZ <karliszakis13@gmail.com>
2023-11-14 20:07:22 +0200

    23214

--- matmul.s
+++ matmul.s
@@ -1,71 +1,59 @@
 .text
 .align 2
-.global matmul
-.type matmul, %function
+.global matmul_optimized
+.type matmul_optimized, %function
 
-matmul:
-    stmfd sp!, {r0-r12, lr}
+matmul_optimized:
+    stmfd sp!, {r4-r11, lr}
 
-    mov r4, #0
-    mov r5, #0
-    mov r6, #0
+    ldr r4, [sp, #0]   @ Load dimensions
+    ldr r5, [sp, #4]
+    ldr r6, [sp, #8]
+
+    mov r0, #0         @ Matrix A address
+    mov r1, #0         @ Matrix B address
+    mov r2, #0         @ Matrix C address
 
 for_i:
-    ldr r2, [sp, #0]
-    cmp r4, r2
+    cmp r0, r4         @ Compare i with rows of A
     bge end_for_i
 
-    mov r5, #0
+    mov r1, #0         @ Reset j to 0 for every i iteration
+    mov r10, r0, LSL #2 @ Calculate base address for matrix C row
 
 for_j:
-    ldr r3, [sp, #56]
-    cmp r5, r3
+    cmp r1, r6         @ Compare j with columns of B
     bge end_for_j
 
-    mov r6, #0
+    mov r3, #0         @ Reset k to 0 for every j iteration
+    mov r11, r1, LSL #2 @ Calculate base address for matrix B column
+
+    vldr d0, [r2]!     @ Load C[i][j] to a NEON register
+    add r2, r2, #8     @ Move to the next C element
 
 for_k:
-    ldr r1, [sp, #12]
-    cmp r6, r1
+    cmp r3, r5         @ Compare k with columns of A
     bge end_for_k
 
-    mov r9, r4, LSL #2
-    mul r7, r9, r1
-    mov r10, r6, LSL #2
-    add r7, r7, r10
+    vldr d1, [r0, r3, LSL #2]  @ Load A[i][k] to a NEON register
+    vldr d2, [r1, r3, LSL #3]  @ Load B[k][j] to a NEON register
+    vmla.f32 d0, d1, d2        @ Vector matrix multiplication and accumulation
 
-    ldr r0, [sp, #8]
-    ldr r1, [r0, r7]
+    add r3, r3, #2     @ Move to the next pair of elements in A and B
 
-    mul r7, r10, r3
-    mov r9, r5, LSL #2
-    add r7, r7, r9
-    ldr r0, [sp, #60]
-    ldr r2, [r0, r7]
-
-    mul r11, r1, r2
-
-    ldr r0, [sp, #64]
-    mov r9, r4, LSL #2
-    mul r7, r9, r3
-    mov r12, r5, LSL #2
-    add r7, r7, r12
-
-    ldr r0, [r0, r7]
-    add r1, r11, r0
-    str r1, [r0, r7]
-
-    add r6, r6, #1
     b for_k
 
 end_for_k:
-    add r5, r5, #1
+    vstr d0, [r10]!    @ Store the result C[i][j]
+    add r10, r10, #8   @ Move to the next element in C
+
+    add r1, r1, #1     @ Move to the next column in B
     b for_j
 
 end_for_j:
-    add r4, r4, #1
+    add r0, r0, #4     @ Move to the next row in A
     b for_i
 
 end_for_i:
-    ldmfd sp!, {r0-r12, lr}
+    ldmfd sp!, {r4-r11, lr}
     bx lr
